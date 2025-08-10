d0725fcbc47a657028d00fb7bdb1aa4b310b55b8
brittojo7n <sjbritto2003@mail.com>
2025-08-10 12:14:42 +0530

    updated gmail for nodemailer config using oauth2

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,26 +1,31 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    type: 'OAuth2',
+    user: process.env.OAUTH_USER,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
 });
 
 const sendEmail = (to, subject, html) => {
   const mailOptions = {
-    from: `"Job Portal" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
+    from: `"Job Portal" <${process.env.OAUTH_USER}>`,
+    to: to,
+    subject: subject,
+    html: html,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log('Error sending email:', error);
     }
-    console.log('Email sent: ' + info.response);
+    console.log('Email sent successfully: %s', info.response);
   });
 };
 
